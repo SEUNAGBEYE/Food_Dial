bfdf075b4820a632c5150a3f613209dc81c0b0db
seunagbeye <agbeyeseun1@gmail.com>
2017-07-14 02:32:58 +0100

    static files

--- Food_Dial/settings.py
+++ Food_Dial/settings.py
@@ -132,13 +132,13 @@ USE_TZ = True
 
 
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATIC_URL = '/static/'
 
 # Extra places for collectstatic to find static files.
-STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'static'),
-)
+# STATICFILES_DIRS = (
+#     os.path.join(BASE_DIR, 'static'),
+# )
 
 
 MEDIA_DIR = os.path.join(BASE_DIR , 'media')

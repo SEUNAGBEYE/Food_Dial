bedbe127fa2e917001868c37c41a59b973dcbb2e
seunagbeye <agbeyeseun1@gmail.com>
2017-07-14 02:35:43 +0100

    psycop included

--- Food_Dial/settings.py
+++ Food_Dial/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '9u5_upvq!8#9i&fqmbp+1j88e6-somwkjp8+wuq+1j^*27q9fz'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['swicthfood.herokuapp.com']
 
 
 # Application definition
